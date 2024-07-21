187fe6545ce70afb492020f7ef6ee9737c98e27f
MirzoA <102673274+MirzoA@users.noreply.github.com>
2024-07-21 22:26:38 +0200

    Add files via upload

---
+++ Modul_12_homework_1_unyt_test.py
@@ -0,0 +1,43 @@
+class Runner:
+    def __init__(self, name):
+        self.name = "Eva"
+        self.name = "Anna"
+        self.name = "Janet"
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        eva = Runner("Eva")
+        for _ in range(10):
+            eva.walk()
+        self.assertEqual(eva.distance, 50)
+
+    def test_run(self):
+        eva = Runner("Eva")
+        for _ in range(10):
+            eva.run()
+        self.assertEqual(eva.distance, 100)
+
+    def test_challenge(self):
+        anna = Runner("Anna")
+        jan = Runner("Janet")
+        for _ in range(10):
+            anna.walk()
+            jan.run()
+        self.assertNotEqual(anna.distance, jan.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
